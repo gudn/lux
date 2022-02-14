356367e5b130c2d86d9110a2df1535810fe14e19
Daniil Novoselov <gudnmail@gmail.com>
2022-02-14 22:48:19 +0500

    feat: add exec option to cli

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"syscall"
 
 	"github.com/gudn/lux"
 	flag "github.com/spf13/pflag"
@@ -13,12 +15,14 @@ var (
 	projectSearchPath string
 	configPath        string
 	templatesPath     string
+	execNginx         bool
 )
 
 func init() {
 	flag.StringVarP(&projectSearchPath, "projects", "p", "projects", "path to search projects")
 	flag.StringVarP(&configPath, "out", "o", "config", "path to output config files")
 	flag.StringVarP(&templatesPath, "templates", "t", "templates", "path to search templates")
+	flag.BoolVarP(&execNginx, "exec", "e", false, "execve to nginx master process")
 	flag.Parse()
 }
 
@@ -47,7 +51,27 @@ func main() {
 			projects = append(projects, name)
 		}
 	}
-	_, err = lux.New(projects, templates, configPath)
+	l, err := lux.New(projects, templates, configPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	env := os.Environ()
+	root, err := filepath.Abs(l.RootConfig())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if !execNginx {
+		return
+	}
+	args := []string{"-c", root, "-g", "daemon off;"}
+	nginx, err := exec.LookPath("nginx")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	err = syscall.Exec(nginx, args, env)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
